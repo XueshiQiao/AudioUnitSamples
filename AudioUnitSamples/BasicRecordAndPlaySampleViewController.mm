6366b8e1d037b4a3456d91ce2ffcf7094f2c12fe
Joey <qiaoxueshi@gmail.com>
2022-01-26 16:48:17 +0800

    Try 1 (add render callback and input callback)

--- AudioUnitSamples/BasicRecordAndPlaySampleViewController.mm
+++ AudioUnitSamples/BasicRecordAndPlaySampleViewController.mm
@@ -19,6 +19,7 @@ static const int kBytesPerSample = 2;
 
 @interface BasicRecordAndPlaySampleViewController () {
   AudioUnit _ioUnit;
+  AudioBufferList _buffer;
 }
 
 @end
@@ -27,6 +28,10 @@ static const int kBytesPerSample = 2;
 
 - (void)viewDidLoad {
   [super viewDidLoad];
+  _buffer.mNumberBuffers = 1;
+  _buffer.mBuffers[0].mNumberChannels = 1;
+  _buffer.mBuffers[0].mDataByteSize = 4096;
+  _buffer.mBuffers[0].mData = malloc(_buffer.mBuffers[0].mDataByteSize);
 }
 
 - (IBAction)didTapStartRecordButton:(id)sender {
@@ -36,7 +41,7 @@ static const int kBytesPerSample = 2;
   if (error) {
       NSLog(@"setCategory error:%@", error);
   }
-  [[AVAudioSession sharedInstance] setPreferredIOBufferDuration:0.005 error:&error];
+  [[AVAudioSession sharedInstance] setPreferredIOBufferDuration:0.05 error:&error];
   if (error) {
       NSLog(@"setPreferredIOBufferDuration error:%@", error);
   }
@@ -76,8 +81,56 @@ static OSStatus ioUnitRenderNotify(void *              inRefCon,
 //    // !!! this method is timing sensitive, better not add any wasting time code here, even nslog
 //    if (*ioActionFlags & kAudioUnitRenderAction_PostRender) {
 //    }
-  NSLog(@"====== %@", @(*ioActionFlags));
+    BasicRecordAndPlaySampleViewController *vc = (__bridge BasicRecordAndPlaySampleViewController *)inRefCon;
+    AudioUnitRenderActionFlags flags = *ioActionFlags;
+//    @constant        kAudioUnitRenderAction_PostRenderError
+//                    If this flag is set on the post-render call an error was returned by the
+//                    AUs render operation. In this case, the error can be retrieved through the
+//                    lastRenderError property and the audio data in ioData handed to the post-render
+//                    notification will be invalid.
+
+    if (flags & kAudioUnitRenderAction_PostRenderError) {
+        OSStatus ret;
+        UInt32 size = sizeof(OSStatus);
+//        OSStatus result = AudioUnitGetProperty(_audioUnit,
+//            kAudioUnitProperty_LastRenderError, kAudioUnitScope_Global, 0, &ret, &size);
+//
+        OSStatus status = AudioUnitGetProperty(vc->_ioUnit, kAudioUnitProperty_LastRenderError, kAudioUnitScope_Global, kOutputBus, &ret, &size);
+        NSLog(@"status: %@, ret: %@", @(status), @(ret));
+    }
     return noErr;
+}
+
+static OSStatus OnGetPlayoutData(void *              inRefCon,
+                                      AudioUnitRenderActionFlags *  ioActionFlags,
+                                      const AudioTimeStamp *      inTimeStamp,
+                                      UInt32              inBusNumber,
+                                      UInt32              inNumberFrames,
+                                      AudioBufferList *        ioData) {
+  BasicRecordAndPlaySampleViewController *vc = (__bridge BasicRecordAndPlaySampleViewController *)inRefCon;
+  AudioUnitRenderActionFlags flags = *ioActionFlags;
+  NSLog(@"====== %@", @(flags));
+//  *ioActionFlags |= kAudioUnitRenderAction_OutputIsSilence;
+  for (UInt32 i = 0; i< ioData->mNumberBuffers; ++i) {
+    memcpy(ioData->mBuffers[i].mData, vc->_buffer.mBuffers[i].mData, vc->_buffer.mBuffers[i].mDataByteSize);
+    //memset(ioData->mBuffers[i].mData, 0, ioData->mBuffers[i].mDataByteSize);
+  }
+  return noErr;
+}
+
+static OSStatus OnRecordedDataAvailable(void *              inRefCon,
+                                         AudioUnitRenderActionFlags *  ioActionFlags,
+                                         const AudioTimeStamp *      inTimeStamp,
+                                         UInt32              inBusNumber,
+                                         UInt32              inNumberFrames,
+                                         AudioBufferList *        ioData) {
+  BasicRecordAndPlaySampleViewController *vc = (__bridge BasicRecordAndPlaySampleViewController *)inRefCon;
+  OSStatus status = AudioUnitRender(vc->_ioUnit, ioActionFlags, inTimeStamp, inBusNumber, inNumberFrames, &vc->_buffer);
+  if (status != noErr) {
+    NSLog(@"AudioUnitRender error: %@", @(status));
+  }
+  return status;
+  
 }
 
 - (AudioStreamBasicDescription)getFormat {
@@ -147,21 +200,21 @@ static OSStatus ioUnitRenderNotify(void *              inRefCon,
     return false;
   }
 
-//  // Specify the callback function that provides audio samples to the audio
-//  // unit.
-//  AURenderCallbackStruct render_callback;
-//  render_callback.inputProc = OnGetPlayoutData;
-//  render_callback.inputProcRefCon = this;
-//  result = AudioUnitSetProperty(
-//      _ioUnit, kAudioUnitProperty_SetRenderCallback, kAudioUnitScope_Input,
-//      kOutputBus, &render_callback, sizeof(render_callback));
-//  if (result != noErr) {
-////    DisposeAudioUnit();
-////    RTCLogError(@"Failed to specify the render callback on the output bus. "
-////                 "Error=%ld.",
-////                (long)result);
-//    return false;
-//  }
+  // Specify the callback function that provides audio samples to the audio
+  // unit.
+  AURenderCallbackStruct render_callback;
+  render_callback.inputProc = OnGetPlayoutData;
+  render_callback.inputProcRefCon = (__bridge void *)self;
+  result = AudioUnitSetProperty(
+      _ioUnit, kAudioUnitProperty_SetRenderCallback, kAudioUnitScope_Input,
+      kOutputBus, &render_callback, sizeof(render_callback));
+  if (result != noErr) {
+//    DisposeAudioUnit();
+//    RTCLogError(@"Failed to specify the render callback on the output bus. "
+//                 "Error=%ld.",
+//                (long)result);
+    return false;
+  }
 
   // Disable AU buffer allocation for the recorder, we allocate our own.
   // TODO(henrika): not sure that it actually saves resource to make this call.
@@ -177,16 +230,16 @@ static OSStatus ioUnitRenderNotify(void *              inRefCon,
     return false;
   }
 
-  // Specify the callback to be called by the I/O thread to us when input audio
-  // is available. The recorded samples can then be obtained by calling the
-  // AudioUnitRender() method.
-//  AURenderCallbackStruct input_callback;
-//  input_callback.inputProc = OnDeliverRecordedData;
-//  input_callback.inputProcRefCon = this;
-//  result = AudioUnitSetProperty(vpio_unit_,
-//                                kAudioOutputUnitProperty_SetInputCallback,
-//                                kAudioUnitScope_Global, kInputBus,
-//                                &input_callback, sizeof(input_callback));
+//   Specify the callback to be called by the I/O thread to us when input audio
+//   is available. The recorded samples can then be obtained by calling the
+//   AudioUnitRender() method.
+  AURenderCallbackStruct input_callback;
+  input_callback.inputProc = OnRecordedDataAvailable;
+  input_callback.inputProcRefCon = (__bridge void*)self;
+  result = AudioUnitSetProperty(_ioUnit,
+                                kAudioOutputUnitProperty_SetInputCallback,
+                                kAudioUnitScope_Global, kInputBus,
+                                &input_callback, sizeof(input_callback));
   return true;
 }
 
