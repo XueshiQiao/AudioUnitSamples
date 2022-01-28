e8441e0e1b12f20dac80b4210cb4efcbebb428b2
Joey <qiaoxueshi@gmail.com>
2022-01-29 00:05:36 +0800

    Move C function implementations into .mm file

--- AudioUnitSamples/Common/CommonUtils.h
+++ AudioUnitSamples/Common/CommonUtils.h
@@ -38,6 +38,18 @@ inline OSStatus CheckErrorStatus(OSStatus status, const std::string& operation) 
   return status;
 }
 
+void MixSInt16AudioSamples(SInt16 *dst, SInt16 *src,
+                           float dst_factor, float src_factor,
+                           size_t size_in_bytes);
+
+bool MixAudioBufferList(AudioBufferList& dst, const AudioBufferList& src,
+                        float dst_factor, float src_factor);
+
+void InitAudioBufferList(AudioBufferList* audio_buffer);
+
+void DisposeAudioBufferList(AudioBufferList* audio_buffer);
+
+bool CopyAudioBufferListDatas(AudioBufferList& dst, const AudioBufferList& src);
 
 @interface CommonUtils : NSObject
 
@@ -48,60 +60,5 @@ inline OSStatus CheckErrorStatus(OSStatus status, const std::string& operation) 
 + (BOOL)setupAudioSessionForCategory:(AVAudioSessionCategory)category;
 
 @end
-
-static void MixSInt16AudioSamples(SInt16 *dst, SInt16 *src,
-                                  float dst_factor, float src_factor,
-                                  size_t size_in_bytes) {
-  for (size_t i = 0; i < size_in_bytes / sizeof(SInt16); i++) {
-    *(dst + i) = static_cast<SInt16>(*(dst + i) * dst_factor) +
-                 static_cast<SInt16>(*(src + i) * src_factor);
-  }
-}
-
-static bool MixAudioBufferList(AudioBufferList& dst, const AudioBufferList& src,
-                               float dst_factor, float src_factor) {
-  if (dst.mNumberBuffers != src.mNumberBuffers ||
-      (dst.mBuffers[0].mDataByteSize != src.mBuffers[0].mDataByteSize)) {
-    return false;
-  }
-  SInt16 *_dst = static_cast<SInt16*>(dst.mBuffers[0].mData);
-  SInt16 *_src = static_cast<SInt16*>(src.mBuffers[0].mData);
-  MixSInt16AudioSamples(_dst, _src, dst_factor, src_factor, src.mBuffers[0].mDataByteSize);
-  return true;
-}
-
-static void InitAudioBufferList(AudioBufferList* audio_buffer) {
-  UInt32 sizeInBytes = 512;
-  audio_buffer->mNumberBuffers = 1;
-  audio_buffer->mBuffers[0].mNumberChannels = [CommonUtils commonRecorderAudioFormat].mChannelsPerFrame;
-  audio_buffer->mBuffers[0].mDataByteSize = sizeInBytes;
-  audio_buffer->mBuffers[0].mData = malloc(sizeInBytes);
-  memset(audio_buffer->mBuffers[0].mData, 0, sizeInBytes);
-}
-
-static void DisposeAudioBufferList(AudioBufferList* audio_buffer) {
-  for (size_t i = 0; i < audio_buffer->mNumberBuffers; i++) {
-    if (audio_buffer->mBuffers[0].mData) {
-      free(audio_buffer->mBuffers[0].mData);
-    }
-  }
-}
-
-static bool CopyAudioBufferListDatas(AudioBufferList& dst, const AudioBufferList& src) {
-  if (dst.mNumberBuffers != src.mNumberBuffers) {
-    return false;
-  }
-  
-  for (size_t i = 0; i < dst.mNumberBuffers; i++) {
-    if (dst.mBuffers[i].mDataByteSize >= src.mBuffers[0].mDataByteSize) {
-      memcpy(dst.mBuffers[i].mData, src.mBuffers[0].mData, src.mBuffers[0].mDataByteSize);
-    } else {
-      return false;
-    }
-  }
-
-  return true;
-}
-
 
 NS_ASSUME_NONNULL_END
--- AudioUnitSamples/Common/CommonUtils.mm
+++ AudioUnitSamples/Common/CommonUtils.mm
@@ -7,6 +7,60 @@
 
 #import "CommonUtils.h"
 
+void MixSInt16AudioSamples(SInt16 *dst, SInt16 *src,
+                           float dst_factor, float src_factor,
+                           size_t size_in_bytes) {
+  for (size_t i = 0; i < size_in_bytes / sizeof(SInt16); i++) {
+    *(dst + i) = static_cast<SInt16>(*(dst + i) * dst_factor) +
+                 static_cast<SInt16>(*(src + i) * src_factor);
+  }
+}
+
+bool MixAudioBufferList(AudioBufferList& dst, const AudioBufferList& src,
+                               float dst_factor, float src_factor) {
+  if (dst.mNumberBuffers != src.mNumberBuffers ||
+      (dst.mBuffers[0].mDataByteSize != src.mBuffers[0].mDataByteSize)) {
+    return false;
+  }
+  SInt16 *_dst = static_cast<SInt16*>(dst.mBuffers[0].mData);
+  SInt16 *_src = static_cast<SInt16*>(src.mBuffers[0].mData);
+  MixSInt16AudioSamples(_dst, _src, dst_factor, src_factor, src.mBuffers[0].mDataByteSize);
+  return true;
+}
+
+void InitAudioBufferList(AudioBufferList* audio_buffer) {
+  UInt32 sizeInBytes = 512;
+  audio_buffer->mNumberBuffers = 1;
+  audio_buffer->mBuffers[0].mNumberChannels = [CommonUtils commonRecorderAudioFormat].mChannelsPerFrame;
+  audio_buffer->mBuffers[0].mDataByteSize = sizeInBytes;
+  audio_buffer->mBuffers[0].mData = malloc(sizeInBytes);
+  memset(audio_buffer->mBuffers[0].mData, 0, sizeInBytes);
+}
+
+void DisposeAudioBufferList(AudioBufferList* audio_buffer) {
+  for (size_t i = 0; i < audio_buffer->mNumberBuffers; i++) {
+    if (audio_buffer->mBuffers[0].mData) {
+      free(audio_buffer->mBuffers[0].mData);
+    }
+  }
+}
+
+bool CopyAudioBufferListDatas(AudioBufferList& dst, const AudioBufferList& src) {
+  if (dst.mNumberBuffers != src.mNumberBuffers) {
+    return false;
+  }
+  
+  for (size_t i = 0; i < dst.mNumberBuffers; i++) {
+    if (dst.mBuffers[i].mDataByteSize >= src.mBuffers[0].mDataByteSize) {
+      memcpy(dst.mBuffers[i].mData, src.mBuffers[0].mData, src.mBuffers[0].mDataByteSize);
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 @implementation CommonUtils
 
 + (AudioStreamBasicDescription)commonRecorderAudioFormat {
